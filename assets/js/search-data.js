8fdc09009756d6a997e74936c2d5e256165854cd
RickWayne1125 <44436897+RickWayne1125@users.noreply.github.com>
2025-09-28 03:57:16 +0000

    Deploying to gh-pages from @ RickWayne1125/RickWayne1125.github.io@0f4f0c53e6f2343c03ef708ea0308416a19d5970 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,13 +37,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "nav-teaching",
           title: "teaching",
           description: "Materials for courses you taught. Replace this text with your description.",
