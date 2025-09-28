536376f05ec0f36ed3d7544e947fca11b17409e5
RickWayne1125 <44436897+RickWayne1125@users.noreply.github.com>
2025-09-28 20:57:25 +0000

    Deploying to gh-pages from @ RickWayne1125/RickWayne1125.github.io@934993594254c70393d67a4c50e3bf0a0c1cf736 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,14 +51,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%72%77%65%69%37@%73%74%65%76%65%6E%73.%65%64%75", "_blank");
         },
       },{
         id: 'social-rss',
@@ -66,20 +59,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
